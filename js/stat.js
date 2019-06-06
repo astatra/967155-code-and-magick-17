7942179b75221635fed9080340b31d99f9611f11
Hanna Rul <anna.astatra@gmail.com>
2019-06-06 23:01:44 +0200

    Second task

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,12 @@
 'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_PADDING_X = 140;
 var GAP = 10;
+var FONT_SIZE = 16;
 var COLUMN_WIDTH = 40;
 var COLUMN_GAP = 50;
 var BAR_WIDTH = 40;
@@ -29,27 +32,31 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+  var titleBottom = 60;
   ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
+  ctx.font = FONT_SIZE + 'px PT Mono';
   ctx.fillText('Ура вы победили!', 130, 40);
-  ctx.fillText('Список результатов:', 130, 60);
+  ctx.fillText('Список результатов:', 130, titleBottom);
 
   var maxTime = getMaxElement(times);
 
-  for (var i = 0; i < names.length; i++) {
-    var timeSpent = times[i];
+  names.forEach(function (playersName, index) {
+    var timeSpent = times[index];
     var rank = timeSpent / maxTime;
     var currentBarHeight = rank * MAX_BAR;
 
-    if (names[i] === 'Вы') {
+    var currentResultX = CLOUD_PADDING_X + (COLUMN_WIDTH + COLUMN_GAP) * index;
+    var currentResultY = titleBottom + FONT_SIZE + GAP * 2 + MAX_BAR - currentBarHeight;
+
+    if (playersName === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(31, 58, 147,' + rank + ')';
     }
 
-    ctx.fillRect(140 + (COLUMN_WIDTH + COLUMN_GAP) * i, (CLOUD_X - GAP) + (MAX_BAR - currentBarHeight), BAR_WIDTH, currentBarHeight);
+    ctx.fillRect(currentResultX, currentResultY, BAR_WIDTH, currentBarHeight);
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), 140 + (COLUMN_WIDTH + COLUMN_GAP) * i, (CLOUD_HEIGHT - currentBarHeight) - 40);
-    ctx.fillText(names[i], 140 + (COLUMN_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - GAP);
-  }
+    ctx.fillText(Math.round(timeSpent), currentResultX, currentResultY - GAP);
+    ctx.fillText(playersName, currentResultX, currentResultY + currentBarHeight + FONT_SIZE);
+  });
 };
